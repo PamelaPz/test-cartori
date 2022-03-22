3b3c6e4bbba14dbf5840189554a601d3f8bcd36f
PamelaPz <jpamelapz02@gmail.com>
2022-03-22 17:47:54 -0600

    iniciando login

---
+++ src/views/LogIn.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login flex">
+    <div class="wp-logo flex">
+      <div>
+        <img src="@/assets/logo.png" alt="logo" srcset="">
+      </div>
+    </div>
+    <div class="wp-btnLogin flex">
+      <router-link to="/about">
+        <div class="content-btn">
+          <img src="@/assets/img/logo-g.png" alt="google" srcset="">
+          <p>Login with Google</p>
+        </div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+  components: {
+  }
+}
+</script>
